476cf7c581a9444115ce95d30f4ef62044996b50
August Ollé <august@augustolle.com>
2023-08-26 14:57:04 +0200

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react'
-import { useFetchPokemon } from './hooks/useFetchPokemon';
 import SearchSection from './components/SearchSection/SearchSection';
 import PokemonName from './components/PokemonName/PokemonName';
 import Sprite from './components/sprite/Sprite';
@@ -11,26 +10,26 @@ import { API_URL, defaultPokemon } from '../appConfig';
 function App() {
 
   const [pokemonName, setPokemonName] = useState(defaultPokemon)
-
-  useEffect(() => {
-    handleClick(pokemonName)
-  }, [pokemonName])
-
   const [ data, setData ] = useState(null)
   const [ isLoading, setIsLoading ] = useState(false)
   const [ error, setError ] = useState(null)
 
-  const handleClick = async (pokemon) => {
-    console.log ('fetching...')
+  useEffect(() => {
+    fetchPokemon(pokemonName)
+  }, [pokemonName])
+
+  const fetchPokemon = async (pokemon) => {
+
     setIsLoading(true)
 
     try {
       const response = await fetch(API_URL + pokemon)
-
       if (response.status !== 200) {
         throw new Error(`Error status: ${response.status}`)
       }
       const result = await response.json();
+      setData(null)
+      setError(null)
       setData(result)  
     }
     catch (error) {
@@ -44,23 +43,19 @@ function App() {
   return (
     <main className='app-container'>
           <div className="app-section">
-            <SearchSection setPokemonName={setPokemonName} />
-            <PokemonName name={ pokemonName } isLoading={ isLoading } error={ error }/>
+            <SearchSection setPokemonName={ setPokemonName } error={ error }/>
+            <PokemonName name={ pokemonName } isLoading={ isLoading } data={data}/>
           </div>
-          {data &&(
-            <>
-            <div className='app-section sprite-section'> 
-              <Sprite data={ data } />
-            </div>
-            <div className='app-section attribute-section'> 
-              <Attributes data={ data } isLoading={ isLoading } error={ error }/>
-            </div>
-            <div className='app-section'> 
-              <Stats data={ data } isLoading={ isLoading } error={ error }/>
-            </div>
-          </>
-          )}
-</main>
+          <div className='app-section sprite-section'> 
+            <Sprite data={ data } />
+          </div>
+          <div className='app-section attribute-section'> 
+            <Attributes data={ data }/>
+          </div>
+          <div className='app-section stats-section'> 
+            <Stats data={ data } isLoading={ isLoading } error={ error }/>
+          </div>
+    </main>
   )
 }
 
