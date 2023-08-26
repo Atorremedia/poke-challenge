bdddf7d06c3647ea47d2bbfa28c5c4a6b296767a
August Ollé <august@augustolle.com>
2023-08-26 15:00:04 +0200

    styles added

--- src/components/SearchSection/SearchSection.jsx
+++ src/components/SearchSection/SearchSection.jsx
@@ -1,26 +1,38 @@
-import React, { useRef, useState } from 'react'
-import { Input, Typography } from 'antd';
-import { useFetchPokemon } from '../../hooks/useFetchPokemon';
-import { API_URL } from '../../../appConfig';
+import React, { useRef } from 'react'
+import { Input, Typography, Alert } from 'antd';
+import './SearchSection.css'
 
-function SearchSection({setPokemonName}) {
+
+function SearchSection({setPokemonName, error}) {
 
   const inputRef = useRef()
 
   function onSearch(){
-    console.log(inputRef.current.input.value)
-    setPokemonName(inputRef.current.input.value)
+    let search = inputRef.current.input.value.toLowerCase().trimStart().trimEnd()
+    setPokemonName(search)
   }
 
   return (
       <div className="searchbar-container">
+        <Typography.Title level={ 3 }>
+          Choose your Pokémon
+        </Typography.Title>
         <Input.Search
-        placeholder="choose your favorite Pokémon"
+        placeholder="your favorite Pokémon"
         onSearch={onSearch}
         enterButton
         ref={inputRef}
-        
         />
+        <div className="message-container">
+          {error &&
+            <Alert
+            message="error"
+            description="Pokémon not found!"
+            type="error"
+            showIcon
+            />
+          }
+      </div>
       </div>
   )
 }
