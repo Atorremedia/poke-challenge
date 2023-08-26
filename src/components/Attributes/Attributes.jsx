52547910c68dd28b04982288c5e42a95b2d73770
August Ollé <august@augustolle.com>
2023-08-26 14:58:24 +0200

    tags colors and styles added

--- src/components/Attributes/Attributes.jsx
+++ src/components/Attributes/Attributes.jsx
@@ -4,57 +4,55 @@ import { Typography } from 'antd';
 import { types } from '../../../appConfig';
 import './Attributes.css'
 
-let shownAbilities = []
-let shownTypes = []
-let uniqueAbilities = []
-let uniqueTypes = []
 
-function Attributes({data, isLoading, error}) {
+function Attributes({data}) {
 
+  let shownAbilities = []
+  let shownTypes = []
+
+  if(data) {
     data.abilities.map((element) => {
-      shownAbilities.push(<Tag key={element.ability.name}>{element.ability.name} </Tag>)
-      })
+      shownAbilities.push(<Tag key={element.ability.name} style={{fontSize: 16, fontWeight: 600}}>{element.ability.name.toUpperCase()} </Tag>)
+    })
     data.types.map((element) => {
       let type = element.type.name
-      shownTypes.push(<Tag key={element.type.name}  className='type-tag'>{element.type.name}<span><img src={types[type].icon} className='type-icon'></img></span></Tag>)
-      })
-
-
-
+      shownTypes.push(<Tag key={element.type.name} color={types[type].bgColor} style={{fontSize: 18}} className='type-tag' >{element.type.name}<span><img  className='type-icon' src={types[type].icon}></img></span></Tag>)
+    })
+  }
 
   return (
-    <div className='app-section attribute-section'> 
+    <div className='app-section attribute-section'>
 {
   data && (
     <>
       <div className="attribute-name">
         <Typography.Text>ID</Typography.Text>
       </div>
-      <div className="attribute-content">
-        <Typography.Text>{data.id}</Typography.Text>
+      <div className="attribute-value">
+        <Typography.Text>#{data.id}</Typography.Text>
       </div>
       <div className="attribute-name">
         <Typography.Text>Height</Typography.Text>
       </div>
-      <div className="attribute-content">
-        <Typography.Text>{data.height}</Typography.Text>
+      <div className="attribute-value">
+        <Typography.Text>{data.height/10} m ( {Math.round(data.height/3.28084)}"{Math.round(data.height%3.28084*12)}' )</Typography.Text>
       </div>
       <div className="attribute-name">
         <Typography.Text>Weight</Typography.Text>
       </div>
-      <div className="attribute-content">
-        <Typography.Text>{data.weight}</Typography.Text>
+      <div className="attribute-value">
+        <Typography.Text>{data.weight/10} kg ( {(data.weight*0.220462).toFixed(1)} lbs )</Typography.Text>
       </div>
       <div className="attribute-name">
         <Typography.Text>Abilities</Typography.Text>
       </div>
-      <div className="attribute-content">
+      <div className="attribute-value">
           {shownAbilities}
       </div>
       <div className="attribute-name">
         <Typography.Text>Type</Typography.Text>
       </div>
-      <div className="attribute-content">
+      <div className="attribute-value">
         {shownTypes}
       </div>
       </>
