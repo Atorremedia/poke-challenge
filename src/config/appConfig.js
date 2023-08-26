73919c4eaf22aaef4aa15ae8485e1ea6d4f5950a
August Ollé <august@augustolle.com>
2023-08-27 01:10:08 +0200

    packages updated

---
+++ src/config/appConfig.js
@@ -0,0 +1,86 @@
+// URL to fetch for a certain Pokémon
+
+export const API_URL = `https://pokeapi.co/api/v2/pokemon/`
+
+// default Pokémon on load
+
+export const defaultPokemon = `bulbasaur`
+
+// Pokémon types, with their icon, background color and font color for type tags
+
+export const types =
+ {
+    "bug": {
+        icon: 'https://static.wikia.nocookie.net/my-singing-monsters-ideas/images/4/48/Bug_Type_Icon.svg.png',
+        bgColor: '#83C300',
+    },
+    "dark": {
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Pok%C3%A9mon_Dark_Type_Icon.svg/240px-Pok%C3%A9mon_Dark_Type_Icon.svg.png',
+        bgColor: '#5B5366',
+    },
+    "dragon": {
+        icon: 'https://static.wikia.nocookie.net/my-singing-monsters-ideas/images/c/c9/Dragon_Type_Icon.svg.png',
+        bgColor: '#006FC9',
+    },
+    "electric": {
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Pok%C3%A9mon_Electric_Type_Icon.svg/240px-Pok%C3%A9mon_Electric_Type_Icon.svg.png',
+        bgColor: '#FBD200',
+    },
+    "fairy": {
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Pok%C3%A9mon_Fairy_Type_Icon.svg/240px-Pok%C3%A9mon_Fairy_Type_Icon.svg.png',
+        bgColor: '#FB8AEC',
+    },
+    "fighting": {
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Pok%C3%A9mon_Fighting_Type_Icon.svg/240px-Pok%C3%A9mon_Fighting_Type_Icon.svg.png',
+        bgColor: '#E12C6A',
+    },
+    "fire": {
+        icon: 'https://static.wikia.nocookie.net/my-singing-monsters-ideas/images/2/25/Fire_Type_Icon.svg.png',
+        bgColor: '#FF9741',
+    },
+    "flying": {
+        icon: 'https://static.wikia.nocookie.net/my-singing-monsters-ideas/images/9/9b/Flying_Type_Icon.svg.png',
+        bgColor: '#89AAE3',
+    },
+    "grass": {
+        icon: 'https://static.wikia.nocookie.net/my-singing-monsters-ideas/images/6/6a/Grass_Type_Icon.svg.png',
+        bgColor: '#38BF4B',
+    },
+    "ghost": {
+        icon: 'https://static.wikia.nocookie.net/my-singing-monsters-ideas/images/e/e2/Ghost_Type_Icon.svg.png',
+        bgColor: '#4C6AB2',
+    },
+    "ground": {
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Pok%C3%A9mon_Ground_Type_Icon.svg/240px-Pok%C3%A9mon_Ground_Type_Icon.svg.png',
+        bgColor: '#E97333',
+    },
+    "ice": {
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Pok%C3%A9mon_Ice_Type_Icon.svg/240px-Pok%C3%A9mon_Ice_Type_Icon.svg.png',
+        bgColor: '#4BD2C1',
+    },
+    "normal": {
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Pok%C3%A9mon_Normal_Type_Icon.svg/240px-Pok%C3%A9mon_Normal_Type_Icon.svg.png',
+        bgColor: '#A0A2A0',
+    },
+    "poison": {
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Pok%C3%A9mon_Poison_Type_Icon.svg/240px-Pok%C3%A9mon_Poison_Type_Icon.svg.png',
+        bgColor: '#B667CF',
+    },
+    "psychic": {
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Pok%C3%A9mon_Psychic_Type_Icon.svg/240px-Pok%C3%A9mon_Psychic_Type_Icon.svg.png',
+        bgColor: '#FF6676',
+    },
+    "rock": {
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Pok%C3%A9mon_Rock_Type_Icon.svg/240px-Pok%C3%A9mon_Rock_Type_Icon.svg.png',
+        bgColor: '#C9B787',
+    },
+    "steel": {
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Pok%C3%A9mon_Steel_Type_Icon.svg/240px-Pok%C3%A9mon_Steel_Type_Icon.svg.png',
+        bgColor: '#5A8FA3',
+    },
+    "water": {
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Pok%C3%A9mon_Water_Type_Icon.svg/240px-Pok%C3%A9mon_Water_Type_Icon.svg.png',
+        bgColor: '#3393DD',
+    }
+    
+}
